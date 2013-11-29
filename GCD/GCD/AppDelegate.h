43b95f99446d305de83b0064b78a1bf53950c710
mike <275712575@qq.com>
2013-11-29 09:22:01 +0800

    添加项目

---
+++ GCD/GCD/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GCD
+//
+//  Created by song on 13-11-29.
+//  Copyright (c) 2013年 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
